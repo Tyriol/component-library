6180d81d7d69c2f4a971c69167c81653a5c4008a
Ryan <ryanshaunsmith@gmail.com>
2025-04-08 14:48:56 +0200

    add basic badge component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Badge from "./components/Badge/Badge";
 
 function App() {
-  return <h1>The Beginning</h1>;
+  return (
+    <>
+      <h1>Component Library</h1>
+      <h2>Badges</h2>
+      <Badge />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Badge.module.css";
+
+export default function Badge(): React.ReactNode {
+  return <span className={styles.badge}>Badge</span>;
+}
