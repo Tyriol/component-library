fa717a24a3e24d59b711f55e649ae7391cf6602d
Ryan <ryanshaunsmith@gmail.com>
2025-04-17 15:49:11 +0100

    add logic for variant style and colour

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,24 @@ function App() {
       <main>
         <Section title="Badges">
           <SectionRow rowTitle="Rounded">
-            <Badge />
+            <Badge variant="rounded" />
+            <Badge variant="rounded" colour="red" />
+            <Badge variant="rounded" colour="yellow" />
+            <Badge variant="rounded" colour="green" />
+            <Badge variant="rounded" colour="blue" />
+            <Badge variant="rounded" colour="indigo" />
+            <Badge variant="rounded" colour="purple" />
+            <Badge variant="rounded" colour="pink" />
           </SectionRow>
           <SectionRow rowTitle="Square">
-            <Badge />
+            <Badge variant="square" />
+            <Badge variant="square" colour="red" />
+            <Badge variant="square" colour="yellow" />
+            <Badge variant="square" colour="green" />
+            <Badge variant="square" colour="blue" />
+            <Badge variant="square" colour="indigo" />
+            <Badge variant="square" colour="purple" />
+            <Badge variant="square" colour="pink" />
           </SectionRow>
         </Section>
       </main>
